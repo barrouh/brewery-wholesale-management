89dc332c3aeacde5113c5ef613e1c959b71002f1
Mohamed Barrouh <mohamed.barrouh@gmail.com>
2023-03-26 13:53:16 +0000

    initial project

---
+++ src/test/java/com/barrouh/BreweryWholesaleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barrouh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryWholesaleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
